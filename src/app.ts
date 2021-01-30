3d7df193ab6e1eb110f9d13a32554c56f94dc974
Chris K <ikim1991@gmail.com>
2021-01-30 03:53:01 -0500

    Heroku

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express, {Application} from 'express';
 import http from 'http';
-// import cors from 'cors';
+import cors from 'cors';
 import gameRouter from './routers/game';
 
 const app: Application = express();
 
 app.use(express.json());
-// app.use(cors());
-
+app.use(cors());
 app.use(gameRouter);
+
 
 export default http.createServer(app);
