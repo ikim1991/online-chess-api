25dbb6e8c506683c376963548c4a5b531397bb69
Chris K <ikim1991@gmail.com>
2021-01-29 23:45:26 -0500

    Heroku Deploy

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express, {Application} from 'express';
 import http from 'http';
-import cors from 'cors';
+// import cors from 'cors';
 import gameRouter from './routers/game';
 
 const app: Application = express();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 app.use(gameRouter);
 
